af371c505f7430e9d529c3b19496f968ca1a902c
twr143 <IVolynin@one.kz>
2023-07-10 14:52:26 +0300

    upd2

--- src/main/scala/integrations/grpc/Client.scala
+++ src/main/scala/integrations/grpc/Client.scala
@@ -1,4 +1,5 @@
 package integrations.grpc
+
 import cats.implicits._
 import cats.effect.{ExitCode, IO, IOApp, Resource}
 import io.grpc.netty.shaded.io.grpc.netty.NettyChannelBuilder
@@ -6,12 +7,14 @@ import fs2.grpc.syntax.all._
 import io.grpc.ManagedChannel
 import grpc.model.hello._
 import io.grpc._
+import protomodels.person.Person.PhoneNumber
+import protomodels.person.{Person, PersonAddressServiceFs2Grpc}
 
 import scala.concurrent.ExecutionContext.Implicits.global
 
 /**
-  * Created by Ilya Volynin on 02.12.2020 at 12:15.
-  */
+ * Created by Ilya Volynin on 02.12.2020 at 12:15.
+ */
 object Client extends IOApp {
   val address = "127.0.0.1:1234"
   val managedChannelResource: Resource[IO, ManagedChannel] =
@@ -21,7 +24,7 @@ object Client extends IOApp {
       .usePlaintext()
       .resource[IO]
 
-  def runProgram(helloStub: GreeterFs2Grpc[IO, Metadata]): IO[Unit] = {
+  def runProgram(helloStub: GreeterFs2Grpc[IO, Metadata], addrBookServ: PersonAddressServiceFs2Grpc[IO, Metadata]): IO[Unit] = {
     for {
       response <- helloStub.sayHello(HelloRequest("Ilyusha V"), new Metadata())
       _ <- IO(println(response.greeting))
@@ -36,11 +39,19 @@ object Client extends IOApp {
         .map(r => println(r.greeting))
         .compile
         .drain
-
+      addResp <- addrBookServ.saveAddress(Person("me", 1, Some("1@1.com"), Seq(PhoneNumber(number = "7777777777"))), new Metadata())
+      _ <- IO(println(addResp.code + " " + addResp.message))
     } yield ()
   }
 
   def run(args: scala.List[String]): cats.effect.IO[ExitCode] =
-    managedChannelResource.use(GreeterFs2Grpc.stubResource[IO](_).use(runProgram)).map( _ => ExitCode.Success)
+    managedChannelResource.use { mcr =>
+      val rs = for {
+        greeter <- GreeterFs2Grpc.stubResource[IO](mcr)
+        addrBookServ <- PersonAddressServiceFs2Grpc.stubResource[IO](mcr)
+      } yield (greeter, addrBookServ)
+
+      rs.use(r => runProgram(r._1, r._2))
+    }.map(_ => ExitCode.Success)
 
 }
--- src/main/scala/integrations/grpc/Server.scala
+++ src/main/scala/integrations/grpc/Server.scala
@@ -1,8 +1,9 @@
 package integrations.grpc
 
 /**
-  * Created by Ilya Volynin on 02.12.2020 at 9:12.
-  */
+ * Created by Ilya Volynin on 02.12.2020 at 9:12.
+ */
+
 import cats.effect.{ExitCode, IO, IOApp, Resource}
 import com.typesafe.scalalogging.StrictLogging
 import fs2.Stream
@@ -10,8 +11,7 @@ import fs2.grpc.syntax.all._
 import io.grpc._
 import grpc.model.hello._
 import io.grpc.netty.shaded.io.grpc.netty.NettyServerBuilder
-
-import scala.concurrent.duration.{DurationInt, FiniteDuration}
+import protomodels.person.{Person, PersonAddressServiceFs2Grpc, PersonResponse}
 
 object Server extends IOApp with StrictLogging {
   class ExampleImplementation(port: Int) extends GreeterFs2Grpc[IO, Metadata] {
@@ -23,26 +23,41 @@ object Server extends IOApp with StrictLogging {
       request.evalMap(req => sayHello(req, clientHeaders))
     }
   }
-  def helloService(port: Int)  =
-     GreeterFs2Grpc.bindServiceResource(new ExampleImplementation(port))
 
-  def runS(port: Int, service: ServerServiceDefinition)=
+  class AddressBookServiceImpl extends PersonAddressServiceFs2Grpc[IO, Metadata] {
+    override def saveAddress(request: Person, ctx: Metadata): IO[PersonResponse] = IO {
+      PersonResponse(code = 0, message = request.name)
+    }
+  }
+
+  def helloService(port: Int): Resource[IO, ServerServiceDefinition] =
+    GreeterFs2Grpc.bindServiceResource(new ExampleImplementation(port))
+
+  def addressService(): Resource[IO, ServerServiceDefinition] =
+    PersonAddressServiceFs2Grpc.bindServiceResource(new AddressBookServiceImpl())
+
+  def runS(port: Int, helloService: ServerServiceDefinition, addressService: ServerServiceDefinition) =
     NettyServerBuilder
       .forPort(port)
-      .addService(service)
+      .addService(helloService)
+      .addService(addressService)
       .resource[IO]
       .evalMap(server => IO(server.start()))
       .use(_ => IO {
-                    logger.warn(s"${System.getProperty("os.name")} Press Ctrl+Z to exit...")
-                    while (System.in.read() != -1) {}
-                    logger.warn("Received end-of-file on stdin. Exiting")
-                    ExitCode.Success
+        logger.warn(s"${System.getProperty("os.name")} Press Ctrl+Z to exit...")
+        while (System.in.read() != -1) {}
+        logger.warn("Received end-of-file on stdin. Exiting")
+        ExitCode.Success
       })
 
   def run(args: scala.List[String]): cats.effect.IO[cats.effect.ExitCode] = {
     args.size match {
       case 1 =>
-        helloService(args(0).toInt).use(s => runS(args(0).toInt, s))
+        val rs = for {
+          addr <- addressService()
+          hello <- helloService(args.head.toInt)
+        } yield (addr, hello)
+        rs.use(s => runS(args.head.toInt, s._1, s._2))
       case _ =>
         IO {
           logger.warn("Please provide a port as an argument. Exiting")
